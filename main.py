991e05b04cad63959a2df8a80a795cb02d53177e
AnikoCh <96246110+AnikoCh@users.noreply.github.com>
2022-11-23 15:10:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,25 +3,25 @@ import time
 from time import mktime
 
 print("Discord Rich Presence")
-client_id = ''  # Your application's client ID as a string. (This isn't a real client ID)
+client_id = '1044518339915022389'  # Your application's client ID as a string. (This isn't a real client ID)
 
 time.sleep(1)
 start_time = mktime(time.localtime())
 while True:
     activity = {
-            "state": "2nd Header",  # anything you like
-            "details": "1st Header",  # anything you like
+            "state": "Please trust me",  # anything you like
+            "details": "Nah bro im not suspicious guy",  # anything you like
             "timestamps": {
                 "start": start_time
             },
             "assets": {
-                "small_text": "Small Image Hover Text",  # anything you like
-                "small_image": "Small Image Key",  # must match the image key
-                "large_text": "Large Image Hover Text",  # anything you like
-                "large_image": "Large Image Key"  # must match the image key
+                "small_text": "Jett",  # anything you like
+                "small_image": "default",  # must match the image key
+                "large_text": "Jett Pussy",  # anything you like
+                "large_image": "default"  # must match the image key
             },
             "buttons": [
-                {"label": "Button 1 Text", "url": "https://www.google.com"},
+                {"label": "Press the pussy", "url": "https://www.google.com"},
             ]
         }
     try:
